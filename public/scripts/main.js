e4539437c7622655db877b46d211980c99f6cb13
Uener <uenercoelho2@hotmail.com>
2021-09-16 19:36:31 -0400

    Modal chages

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,15 +2,30 @@ import Modal from './modal.js'
 
 const modal = Modal()
 
-// Pegar todos os botões que existem com a class check
+const modalTitle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
+
+// Funcionalidade de Marcar como Lido
 const checkButtons = document.querySelectorAll('.actions a.check')
 
 checkButtons.forEach(button => {
   // adicionar a escuta
-  button.addEventListener('click', event => {
-    // Open Modal
-    modal.open()
-  })
+  button.addEventListener('click', handleClick)
 })
 
-// ===#=== Parei no minuto 01:15h da aula 04 ===#===
+// Funcionalidade de Excluir
+const deleteButton = document.querySelectorAll('.actions a.delete')
+
+deleteButton.forEach(button => {
+  // Escutar o click do excluir
+  button.addEventListener('click', event => handleClick(event, (check = true)))
+})
+
+function handleClick(event, check = true) {
+  modalTitle.innerHTML = check ? 'Marcar como lido?' : 'Excluir essa pergunta?'
+  // Abrir Modal
+  modal.open()
+}
+
+// ===#=== Parei no minuto 1:400h da Aula 04 ===#===
