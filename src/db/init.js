4f513999005c361b1e880d231ae3ab111dec04cb
Uener Coelho <uenercoelho2@hotmail.com>
2021-10-04 11:55:29 -0400

    roquet.q

--- src/db/init.js
+++ src/db/init.js
@@ -6,12 +6,14 @@ const initDb = {
 
     await db.exec(`CREATE TABLE rooms (
           id INTEGER PRIMARY KEY, 
-          pass TEXT)`)
+          pass TEXT
+          )`)
 
-    db.exec(`CREATE TABLE questions (
+    await db.exec(`CREATE TABLE questions (
           id INTEGER PRIMARY KEY AUTOINCREMENT, 
           titulo TEXT,
-          read INT)`)
+          read INT
+          )`)
 
     await db.close()
   }
