625d0f92d4d1694f555e9569c83dab55d3e90fd3
Uener Coelho <uenercoelho2@hotmail.com>
2021-10-27 21:14:54 -0400

    Class 05 Started

--- src/controllers/QuestionController.js
+++ src/controllers/QuestionController.js
@@ -1,3 +1,5 @@
+const Database = require('../db/config')
+
 module.exports = {
   index(req, res) {
     const roomId = req.params.room
@@ -11,5 +13,25 @@ module.exports = {
       action = ${action},
       password = ${password}
       `)
+  },
+
+  async create(req, res) {
+    const db = await Database()
+    const question = req.body.question
+    const roomId = req.params.room
+
+    await db.run(`INSERT INTO questions(
+      title,
+      room,
+      read
+    )VALUES(
+      "${question}",
+      ${roomId},
+      0
+    )`)
+
+    res.redirect(`/room/${roomId}`)
   }
 }
+
+// ===#=== Parei no minuto 00:30 da Aula 05 ===#===
--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -5,25 +5,37 @@ module.exports = {
     const db = await Database()
     const pass = req.body.password
     let roomId
+    let isRoom = true
+    while (isRoom) {
+      // Generate room number
+      for (var i = 0; i < 6; i++) {
+        i == 0
+          ? (roomId = Math.floor(Math.random() * 10).toString())
+          : (roomId += Math.floor(Math.random() * 10).toString())
+      }
 
-    for (var i = 0; i < 6; i++) {
-      i == 0
-        ? (roomId = Math.floor(Math.random() * 10).toString())
-        : (roomId += Math.floor(Math.random() * 10).toString())
+      // Check if room number exist
+      const roomsExistIds = await db.all(`SELECT id FROM rooms`)
+      isRoom = roomsExistIds.some(roomExistId => roomExistId === roomId)
+      if (!isRoom) {
+        //  Add room on DB
+        await db.run(`INSERT INTO rooms (
+          id,
+          pass
+        ) VALUES (
+          ${parseInt(roomId)},
+          ${pass}
+        )`)
+      }
     }
-
-    await db.run(`INSERT INTO rooms (
-      id,
-      pass
-    ) VALUES (
-      ${parseInt(roomId)},
-      ${pass}
-    )`)
 
     await db.close()
 
     res.redirect(`/room/${roomId}`)
+  },
+
+  open(req, res) {
+    const roomId = req.params.room
+    res.render('room', { roomId: roomId })
   }
 }
-
-// ===#=== Parei no início da Aula 05 ===#===
